2a5be1967ef2d3620c4e341612f0449af61a4076
shafali731 <shafaligupta31@gmail.com>
2017-10-29 22:56:28 -0400

    Update Head.js

--- Head.js
+++ Head.js
@@ -1,10 +1,9 @@
-
 function createheader() {
     /*var head = '<div><span class="mastheadnameleft">Editor In Chief: Kevin Boodram and Melissa Hinckly</span>'+
     												'<span class="mastheadnameright">Print Editor: Ahmed Sultan</span></div>'+
     												'<br>'+
     												'<div><span class="mastheadnameleft">Junior Editor In Chief: Chloe Hanson </span>'+
     												'<span class="mastheadnameright">Web Editor: Shaina Peters </span></div><br>';*/
-    var head = "<table><tr><td class='headpart headpartleft'>Editor In Chief: Kevin Boodram and Melissa Hinckly</td><td class='headpart headpartright'>Print Editor: Ahmed Sultan</td></tr><tr><td class='headpart headpartleft'>Junior Editor In Chief: Chloe Hanson</td><td class='headpart headpartright'>Web Editor: Shaina Peters</td></tr></table>";
+    var head = "<table><tr><td class='headpart headpartleft'>Editor In Chief: Melissa Hinckly</td><td class='headpart headpartright'>Print Editor: Shafali Gupta</td></tr><tr><td class='headpart headpartleft'>Junior Editor In Chief: Kevin Boodram</td><td class='headpart headpartright'>Web Editor: Shaina Peters</td></tr></table>";
     document.getElementById("head").innerHTML = head;
 }
